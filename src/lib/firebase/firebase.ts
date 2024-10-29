2bced22ed17ea1b42e75a8a55b2d4af0bd0df057
Juan Andrés Losauro <losaurojuan@gmail.com>
2024-10-29 01:22:39 -0300

    firebase api key

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMlaoxXqqSGS6ncMjA9gz-pB6OTPX3uB8",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "bookstore-df22c.firebaseapp.com",
   projectId: "bookstore-df22c",
   storageBucket: "bookstore-df22c.appspot.com",
